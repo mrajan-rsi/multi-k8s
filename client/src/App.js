d81bd68b2a3dd536ef366ab7fdf21e65afb11a38
mrajan-rsi <52177962+mrajan-rsi@users.noreply.github.com>
2021-09-02 15:14:09 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Fib Calculator!!</h1>
+          <h1 className="App-title">Fib Calculator!!!</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
